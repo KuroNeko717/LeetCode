2c49d789a4b0ab7f37ac092c20e18509d23a99f3
Zoro <61281980+KuroNeko717@users.noreply.github.com>
2023-04-14 14:58:36 +0530

    Add files for LeetCode 2235. Add Two Integers

---
+++ 2235. Add Two Integers/Add_Two_Integers.py
@@ -0,0 +1,14 @@
+#Python3
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+    
+#Python
+# class Solution(object):
+#     def sum(self, num1, num2):
+#         """
+#         :type num1: int
+#         :type num2: int
+#         :rtype: int
+#         """
+#         return num1 + num2;
